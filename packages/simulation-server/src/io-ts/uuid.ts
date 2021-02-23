b0727cff742614fc6d95265ac5d2ee65507b077d
Paul Cowan <paul.cowan@cutting.scot>
2021-02-23 16:15:20 +0000

    io-ts example

--- packages/simulation-server/src/io-ts/uuid.ts
+++ packages/simulation-server/src/io-ts/uuid.ts
@@ -1,9 +1,11 @@
-import { string, brand, Branded } from 'io-ts';
+import * as t from 'io-ts';
+
+const regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
 export interface UUIDBrand {
   readonly UUID: unique symbol;
 }
 
-const re = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+export type uuid = t.Branded<string, UUIDBrand>;
 
-export const uuid = brand(string, (n): n is Branded<string, UUIDBrand> => typeof n === 'string' && re.test(n), 'UUID');
+export const uuid = t.brand(t.string, (s): s is uuid => regex.test(s), 'UUID');
--- packages/simulation-server/src/simulators/auth0/token.test.ts
+++ packages/simulation-server/src/simulators/auth0/token.test.ts
@@ -1,7 +1,26 @@
 import { Token } from './token';
+import { isRight, isLeft } from 'fp-ts/lib/Either';
 
 describe('Token', () => {
-  it('should decode a valid Token', () =>{
-    
-  })
+  it('should decode a valid Token', () => {
+    const token = Token.decode({
+      id: '5cc170e4-b4aa-457b-b7de-06b177ad02ca',
+      email: 'bob@gmail.com',
+      status: 'active',
+      token: 'blah',
+    });
+
+    expect(isRight(token)).toBe(true);
+  });
+
+  it('should fail an invalid token', () => {
+    const token = Token.decode({
+      id: 'no-no-no',
+      email: 'bob@gmail.com',
+      status: 'active',
+      token: 'blah',
+    });
+
+    expect(isLeft(token)).toBe(true);
+  });
 });
